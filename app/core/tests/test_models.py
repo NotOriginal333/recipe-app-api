a86839d572d27cd6fea04d821a0dc8c8fe7312c3
NotOriginal333 <m.naumovych2@gmail.com>
2024-07-18 12:21:13 +0300

    added ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -80,3 +80,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='testtag')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='ingredient1'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
+
